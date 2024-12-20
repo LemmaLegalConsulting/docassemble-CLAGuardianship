4d336679bc8cb5a8bfee88ba4d266c77a4a32f52
Quinten Steenhuis <qsteenhuis@gmail.com>
2024-12-20 16:48:32 -0500

    Replace affidavit concerning care or custody with new "Child Care or Custody Affidavit" (#127)
    * Started adding and replacing variables for new form
    
    * Added PDF file for new form version; renamed and kept old PDF version for temporary reference
    
    * Add standalone test file
    
    * Fix default
    
    * make affidavit new name consistent
    
    * fix esign use on affidavit disclosing care or custody
    
    * Basic addendum
    
    ---------
    
    Co-authored-by: Ramiro Lorca <110207826+RamiLorca@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,12 +4,26 @@ from setuptools import setup, find_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
-standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
-standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+standard_exclude = ("*.pyc", "*~", ".*", "*.bak", "*.swp*")
+standard_exclude_directories = (
+    ".*",
+    "CVS",
+    "_darcs",
+    "./build",
+    "./dist",
+    "EGG-INFO",
+    "*.egg-info",
+)
 
-def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+
+def find_package_data(
+    where=".",
+    package="",
+    exclude=standard_exclude,
+    exclude_directories=standard_exclude_directories,
+):
     out = {}
-    stack = [(convert_path(where), '', package)]
+    stack = [(convert_path(where), "", package)]
     while stack:
         where, prefix, package = stack.pop(0)
         for name in os.listdir(where):
@@ -17,30 +31,28 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
             if os.path.isdir(fn):
                 bad_name = False
                 for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
+                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
                         bad_name = True
                         break
                 if bad_name:
                     continue
-                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                if os.path.isfile(os.path.join(fn, "__init__.py")):
                     if not package:
                         new_package = name
                     else:
-                        new_package = package + '.' + name
-                        stack.append((fn, '', new_package))
+                        new_package = package + "." + name
+                        stack.append((fn, "", new_package))
                 else:
-                    stack.append((fn, prefix + name + '/', package))
+                    stack.append((fn, prefix + name + "/", package))
             else:
                 bad_name = False
                 for pattern in exclude:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
+                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
                         bad_name = True
                         break
                 if bad_name:
                     continue
-                out.setdefault(package, []).append(prefix+name)
+                out.setdefault(package, []).append(prefix + name)
     return out
 
 setup(name='docassemble.CLAGuardianship',
